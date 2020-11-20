daba2a68c78bed83cf3b4ea2984ce10f89632ba4
kompir <42977888+kompir@users.noreply.github.com>
2020-11-20 15:51:55 +0200

    Update server.go

--- server/server.go
+++ server/server.go
@@ -12,11 +12,5 @@ func StartServer(port string)  {
 	http.HandleFunc("/history", services.History)
 	http.HandleFunc("/word", services.TranslateWord)
 	http.HandleFunc("/sentence", services.Sentence)
-
 	log.Fatal(http.ListenAndServe(port, nil))
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
-
 }
